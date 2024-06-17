2601be969b1d3cb04d97335028e50665e07bc194
iassadki <ilias.assadki@gmail.com>
2024-06-17 13:58:20 +0200

    commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, FlatList, StyleSheet, Dimensions, StatusBar, SafeAreaView, ScrollView, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={style = styles.container}>
+      <ScrollView >
+        <Text> Songs </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+function LikedSongsScreen() {
+  return (
+    <View style={style = styles.container}>
+      <Text>Liked Songs</Text>
     </View>
   );
 }
 
+function SongScreen() {
+  return (
+    <View style={style = styles.container}>
+      <Text>Song Infos</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: 'black',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Liked Songs" component={LikedSongsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    color: 'white',
     justifyContent: 'center',
   },
+
 });
